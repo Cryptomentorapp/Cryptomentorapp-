01bc0335c16f629240104b9a1b645afcf62313dc
Cryptomentorapp <cryptomentorapp@gmail.com>
2025-09-13 22:57:25 +0700

    Delete lib directory

--- lib/modules/academy.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-class AcademyPage extends StatelessWidget { const AcademyPage({super.key});
-  @override Widget build(BuildContext c)=> const Center(child: Text('Academy – content placeholder.')); }
