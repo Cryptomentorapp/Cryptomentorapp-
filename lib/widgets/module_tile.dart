3427c715ad6b71161f28c412a67823907712315d
Cryptomentorapp <cryptomentorapp@gmail.com>
2025-09-14 20:21:53 +0700

    Update module_tile.dart

--- lib/widgets/module_tile.dart
+++ lib/widgets/module_tile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../theme/app_theme.dart';
+import '../utils/app_theme.dart';
 class ModuleTile extends StatelessWidget {
   final IconData icon;
   final String line1;
