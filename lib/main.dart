31e6e659a7914e615faa2423989162bf9f5c0439
Cryptomentorapp <cryptomentorapp@gmail.com>
2025-09-14 11:15:57 +0700

    Delete lib directory

--- lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart'; import 'theme.dart'; import 'screens/home_screen.dart';
-void main(){runApp(const CMApp());}
-class CMApp extends StatelessWidget{const CMApp({super.key});
-  @override Widget build(BuildContext c){return MaterialApp(title:'Cryptomentor',debugShowCheckedModeBanner:false,theme:CMTheme.dark(),home:const HomeScreen());}
-}
--- lib/utils/network.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-class Net {
-  static Future<dynamic> getJson(String url) async {
-    final res = await http.get(Uri.parse(url), headers: {'User-Agent': 'Cryptomentor/1.0 (Flutter)'});
-    if (res.statusCode >= 200 && res.statusCode < 300) return jsonDecode(res.body);
-    throw Exception('HTTP ${res.statusCode} for $url');
-  }
-}
