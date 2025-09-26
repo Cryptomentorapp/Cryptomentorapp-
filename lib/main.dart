ed8885f7a064c2e7d65f0e91193f54f9a5a9fcc6
Cryptomentorapp <cryptomentorapp@gmail.com>
2025-09-26 10:56:44 +0700

    Delete lib directory

--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'theme.dart';
-class AppState extends ChangeNotifier{}
-void main(){ runApp(const CMApp()); }
-class CMApp extends StatelessWidget{ const CMApp({super.key});
-  @override Widget build(BuildContext context)=>MaterialApp(title:'Cryptomentor', theme: CMTheme.dark(), home: const Placeholder()); }
--- lib/pages/settings.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-class SettingsPage extends StatelessWidget { const SettingsPage({super.key});
-  @override Widget build(BuildContext context)=>ListView(children: const [
-    ListTile(title: Text('Language'), subtitle: Text('Auto (VI/EN)')),
-    ListTile(title: Text('Theme'), subtitle: Text('Dark')),
-    AboutListTile(applicationName: 'Cryptomentor', applicationVersion: '1.1.0'),
-  ]);
-}
