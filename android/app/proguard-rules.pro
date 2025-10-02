def1a3e80851cc25b8c1c7843e207f2f6d8f841f
Cryptomentorapp <cryptomentorapp@gmail.com>
2025-10-02 17:24:32 +0700

    Add files via upload

---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,3 @@
+// Keep everything by default; adjust later when enabling shrink
+-dontwarn **
+-keep class ** { *; }
---
+++ android/app/src/main/java/com/cryptomentor/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cryptomentor.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // In Flutter projects, this is usually io.flutter.embedding.android.FlutterActivity.
+        // Keeping an empty activity so Android manifest is valid even before Flutter wiring.
+    }
+}
