ab0dd54a6956d5f4ff76f5eab5f84f54fca69c73
Cryptomentorapp <cryptomentorapp@gmail.com>
2025-10-05 10:35:28 +0700

    Delete lib directory

--- lib/screens/launchpad_screen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class LaunchpadScreen extends StatelessWidget {
-  const LaunchpadScreen({super.key});
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: const Text('Launchpad')),
-    body: const Center(child: Text('Coming soon (wired).')),
-  );
-}
